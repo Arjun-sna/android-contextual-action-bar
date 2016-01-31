6111d7326c05b1fa81830edac3da9bcdee9af501
arjun <snarjun92@gmail.com>
2016-01-31 18:38:21 +0530

    gradle change

--- app/src/main/java/com/arj/cabsample/MainActivity.java
+++ app/src/main/java/com/arj/cabsample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.arj.cab;
+package com.arj.cabsample;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.arj.cabsample.R;
 
 public class MainActivity extends AppCompatActivity {
 
