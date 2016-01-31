83d8ef97d72294a84cc16ebed72a5f9297284214
arjun <snarjun92@gmail.com>
2016-01-31 19:50:06 +0530

    bug fixes and icon changes

--- app/src/main/java/com/arj/cabsample/MainActivity.java
+++ app/src/main/java/com/arj/cabsample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.arj.cabsample;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +9,70 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.arj.cab.CabCallback;
+import com.arj.cab.ContextualActionBar;
 import com.arj.cabsample.R;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ContextualActionBar customCab;
+    private CabCallback cabCallback = new CabCallback() {
+        @Override
+        public boolean onCreateCab(ContextualActionBar cab, Menu menu) {
+            return true;
+        }
+
+        @Override
+        public boolean onCabItemClicked(MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.delete_chat_heads:
+                    Toast.makeText(MainActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                    customCab.finish();
+                    return true;
+                case R.id.select_all:
+                    Toast.makeText(MainActivity.this, "Selected all", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.select_none:
+                    Toast.makeText(MainActivity.this, "Deselected all", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public boolean onDestroyCab(ContextualActionBar cab) {
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.materialToolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(customCab == null) {
+                    customCab = new ContextualActionBar(MainActivity.this, R.id.cab_stub)
+                            .setMenu(R.menu.sample_menu)
+                            .setCloseDrawableRes(R.drawable.up_button_white)
+                            .setTitle(getResources().getString(R.string.cab_text))
+                            .setLayoutAnim(R.anim.layout_anim)
+                            .setTitleTypeFace(Typeface.createFromAsset(MainActivity.this.getAssets(), "fonts/Avenir-Medium.ttf"))
+                            .start(cabCallback);
+                } else {
+                    customCab.restore();
+                }
             }
         });
     }
@@ -45,8 +92,18 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_open_cab) {
+            if(customCab == null) {
+                customCab = new ContextualActionBar(MainActivity.this, R.id.cab_stub)
+                        .setMenu(R.menu.sample_menu)
+                        .setCloseDrawableRes(R.drawable.up_button_white)
+                        .setTitle(getResources().getString(R.string.cab_text))
+                        .start(cabCallback);
+            } else {
+                customCab.setTitle(getResources().getString(R.string.cab_text))
+                        .restore();
+            }
+           return true;
         }
 
         return super.onOptionsItemSelected(item);
