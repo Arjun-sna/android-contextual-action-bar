44d2551706060957a085c70b166c48f3ba9b30e1
arjun <snarjun92@gmail.com>
2016-01-31 19:53:45 +0530

    optimisations

--- app/src/main/java/com/arj/cabsample/MainActivity.java
+++ app/src/main/java/com/arj/cabsample/MainActivity.java
@@ -3,19 +3,15 @@ package com.arj.cabsample;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.arj.cab.CabCallback;
 import com.arj.cab.ContextualActionBar;
-import com.arj.cabsample.R;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
